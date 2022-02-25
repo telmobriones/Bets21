caee5105d456f46f041045ba5bee39b8526795e7
Josue Cabezas <josuecr1@gmail.com>
2022-02-25 16:32:01 +0100

    Login

--- src/main/java/gui/LoginGUI.java
+++ src/main/java/gui/LoginGUI.java
@@ -16,8 +16,8 @@ import javax.swing.SwingConstants;
 public class LoginGUI extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField password;
+	private JTextField username;
 
 	/**
 	 * Launch the application.
@@ -39,8 +39,9 @@ public class LoginGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginGUI() {
+		setTitle(ResourceBundle.getBundle("Etiquetas").getString("Login")); //$NON-NLS-1$ //$NON-NLS-2$
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 394);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -52,10 +53,10 @@ public class LoginGUI extends JFrame {
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(91, 58, 274, 29);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		username = new JTextField();
+		username.setBounds(91, 58, 274, 29);
+		contentPane.add(username);
+		username.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("Password"));
 		lblNewLabel_1.setBounds(5, 99, 440, 53);
@@ -63,13 +64,21 @@ public class LoginGUI extends JFrame {
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel_1);
 		
-		textField = new JPasswordField();
-		textField.setBounds(96, 142, 269, 29);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		password = new JPasswordField();
+		password.setBounds(96, 142, 269, 29);
+		contentPane.add(password);
+		password.setColumns(10);
 		
 		JButton btnNewButton = new JButton(ResourceBundle.getBundle("Etiquetas").getString("Login"));
-		btnNewButton.setBounds(5, 217, 440, 53);
+		btnNewButton.setBounds(138, 201, 184, 37);
 		contentPane.add(btnNewButton);
+		
+		JButton btnRegister = new JButton(ResourceBundle.getBundle("Etiquetas").getString("Register")); //$NON-NLS-1$ //$NON-NLS-2$
+		btnRegister.setBounds(266, 284, 117, 25);
+		contentPane.add(btnRegister);
+		
+		JLabel lblIfYouDont = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("RegisterText")); //$NON-NLS-1$ //$NON-NLS-2$
+		lblIfYouDont.setBounds(55, 289, 220, 15);
+		contentPane.add(lblIfYouDont);
 	}
 }
