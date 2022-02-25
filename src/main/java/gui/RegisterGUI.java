64e9bfeae2d98e76a6ffc172e94286fe15e5a21f
Josue Cabezas <josuecr1@gmail.com>
2022-02-25 15:18:43 +0100

    Register

--- src/main/java/gui/RegisterGUI.java
+++ src/main/java/gui/RegisterGUI.java
@@ -13,11 +13,12 @@ import java.awt.Font;
 import java.util.ResourceBundle;
 import javax.swing.SwingConstants;
 
-public class LoginGUI extends JFrame {
+public class RegisterGUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -38,7 +39,7 @@ public class LoginGUI extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public LoginGUI() {
+	public RegisterGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -47,29 +48,40 @@ public class LoginGUI extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("User"));
-		lblNewLabel.setBounds(5, 12, 440, 53);
+		lblNewLabel.setBounds(5, -7, 140, 53);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(91, 58, 274, 29);
+		textField_1.setBounds(152, 5, 274, 29);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("Password"));
-		lblNewLabel_1.setBounds(5, 99, 440, 53);
+		lblNewLabel_1.setBounds(-64, 52, 274, 53);
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		contentPane.add(lblNewLabel_1);
 		
 		textField = new JPasswordField();
-		textField.setBounds(96, 142, 269, 29);
+		textField.setBounds(157, 64, 269, 29);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JButton btnNewButton = new JButton(ResourceBundle.getBundle("Etiquetas").getString("Login"));
 		btnNewButton.setBounds(5, 217, 440, 53);
 		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Password");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1.setFont(new Font("Dialog", Font.PLAIN, 14));
+		lblNewLabel_1_1.setBounds(-146, 108, 440, 53);
+		contentPane.add(lblNewLabel_1_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setColumns(10);
+		passwordField.setBounds(157, 120, 269, 29);
+		contentPane.add(passwordField);
 	}
 }
